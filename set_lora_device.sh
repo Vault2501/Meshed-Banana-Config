36bc8fdf563cd9e3da308c9933f37e050b833f03
greygoo <greygoo@hno3.eu>
2022-10-07 19:43:37 +0200

    Create first menu from config

--- set_lora_device.sh
+++ set_lora_device.sh
@@ -22,11 +22,18 @@ change_value(){
     sed -i.bak "/^  \[\[RNode LoRa Interface/,/^  \[\[/{s/^.*$entry =.*/    port = $evalue/}" $CONFIG 
 }
 
+# read all values from file and fill up array
 for value in $VALUES; do
 	ARR_CONF[$value]=$(get_value $value)
 done
 
-for k in "${!ARR_CONF[@]}"
-do
-  printf "%s\n" "$k=${ARR_CONF[$k]}"
+# create dialog menu
+count=0
+menu=""
+for k in "${!ARR_CONF[@]}"; do
+    count=$(($count + 1))
+    menu="$menu $k ${ARR_CONF[$k]}"
 done
+
+dialog --clear --title "Menu Title" --menu "menu" $((${#ARR_CONF[@]}+7)) 60 ${#ARR_CONF[@]} $menu 
+
